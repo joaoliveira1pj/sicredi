487e1436601c653d61717d1550191686ffbca00e
joaoliveira1pj <157559462+joaoliveira1pj@users.noreply.github.com>
2024-05-06 14:34:17 -0300

    Update sicredi1.py

--- sicredi1.py
+++ sicredi1.py
@@ -1,16 +1,10 @@
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
 
-# Carregar os dados
-agencias = pd.read_excel('./base_agencias.xlsb', sheet_name='base')
-associados = pd.read_excel('./base_associados.xlsb', sheet_name='base')
-indicadores = pd.read_excel('./base_indicadores.xlsb', sheet_name='base')
+agencias = pd.read_excel('./base_agencias.xlsb', sheet_name='base', engine='pyxlsb')
+associados = pd.read_excel('./base_associados.xlsb', sheet_name='base', engine='pyxlsb')
+indicadores = pd.read_excel('./base_indicadores.xlsb', sheet_name='base', engine='pyxlsb')
 
-# Definir a ordem das faixas etárias
-faixas_etarias = ['18-25 anos', '26-30 anos', '31-45 anos', '46-60 anos', 'maiores de 60 anos']
-
-# Definir a função para criar a faixa etária
 def criar_faixa_etaria(idade):
     if idade <= 25:
         return '18-25 anos'
@@ -23,31 +17,25 @@ def criar_faixa_etaria(idade):
     else:
         return 'maiores de 60 anos'
 
-# Aplicar a função para criar a coluna 'Faixa Etária'
 associados['Faixa Etária'] = associados['idade'].apply(criar_faixa_etaria)
 
-# Converter a coluna 'Faixa Etária' para tipo categórico com a ordem desejada
-associados['Faixa Etária'] = pd.Categorical(associados['Faixa Etária'], categories=faixas_etarias, ordered=True)
-
-# Juntar os dados
 dados_completos = pd.merge(pd.merge(associados, indicadores, on=['ano_mes', 'num_conta']), agencias, on=['ano_mes', 'cod_ag'])
 
-# Configurar o estilo do gráfico
-sns.set(style="whitegrid")
+contagem_pessoas_por_agencia = dados_completos.groupby('nome_ag')['num_conta'].nunique()
 
-# Criar o gráfico de barras
-plt.figure(figsize=(12, 8))
-grafico = sns.barplot(x='nome_ag', y='vlr_investimentos', hue='Faixa Etária', data=dados_completos)
-grafico.set_xticklabels(grafico.get_xticklabels(), rotation=45)
+fig, axs = plt.subplots(2, 1, figsize=(10,10))
 
-# Adicionar título e rótulos aos eixos
-plt.title('Investimentos por Agência e Faixa Etária')
-plt.xlabel('Nome da Agência')
-plt.ylabel('Valor dos Investimentos')
+dados_completos.groupby(['nome_ag', 'Faixa Etária'])['vlr_investimentos'].sum().unstack().plot(kind='bar', stacked=True, ax=axs[0])
+axs[0].set_title('Investimentos por Agência e Faixa Etária')
+axs[0].set_xlabel('Nome da Agência')
+axs[0].set_ylabel('Valor dos Investimentos')
+axs[0].tick_params(axis='x', rotation=30)
 
-# Adicionar legenda
-plt.legend(title='Faixa Etária')
+contagem_pessoas_por_agencia.plot(kind='bar', ax=axs[1])
+axs[1].set_title('Número de Pessoas por Agência')
+axs[1].set_xlabel('Nome da Agência')
+axs[1].set_ylabel('Número de Pessoas')
+axs[1].tick_params(axis='x', rotation=30)
 
-# Ajustar layout e mostrar o gráfico
 plt.tight_layout()
 plt.show()
